71a8835da883ca6ec00e8f1bd40e2e6e91854311
Uyen Nhi Quang <111651301+uyennhiquang@users.noreply.github.com>
2022-12-20 06:01:46 -0500

    Add sidebar toggle button

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,10 @@
-const sidebar = document.querySelector('aside');
-const sidebarToggle = document.querySelector('.toggle');
+const sidebar = document.querySelector("aside");
+const sidebarToggle = document.querySelector(".toggle");
 
-sidebarToggle.addEventListener('click', function() {
-  console.log('bananas');
-  
-  if (sidebar.classList.contains('hidden'))
-  {
-    sidebar.classList.remove('hidden');
-  }
-  else
-  {
-    sidebar.classList.add('hidden');
+sidebarToggle.addEventListener("click", function () {
+  if (sidebar.classList.contains("hidden")) {
+    sidebar.classList.remove("hidden");
+  } else {
+    sidebar.classList.add("hidden");
   }
 });
